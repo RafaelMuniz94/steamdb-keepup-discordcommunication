bf75739271d4cba17ca214f4cb0a28c6662e9698
Ramunmi <raruznh@gmail.com>
2023-01-10 23:14:37 -0300

    Structure improvments in map function

--- src/discordcommunication/Services/PayloadMappingService.py
+++ src/discordcommunication/Services/PayloadMappingService.py
@@ -6,39 +6,39 @@ class PayloadMappingService:
         self.Config = config
 
     def Map(self,content):
-        newsList = []
         count = content["count"]
-
-        payload = {}
-        if count <= 10:
-            baseContent = f'Existem {count} notícias sobre o app {content["appid"]} na data de {content["date"]}'
-            for newsitem in content["newsitems"]:
+        payload = []
+        diferencial = ceil(count / 10)
+        for index in range(diferencial):
+            print(f'entrou {index}')
+            baseContent = f'{index+1} de {diferencial} - Existem {count} notícias sobre o app {content["appid"]} na data de {content["date"]}'
+            #partList = content[:times]
+            #for newsIndex in itertools.repeat((10*index),times):
+            indexBase = (10*index)
+            if(indexBase == 0):
+                times = 10
+            else:
+                times = count - indexBase
+            newsList = []
+            for indexInterno in range(0,times):
+                print(index)
+                print(indexInterno)
+                print(indexInterno+indexBase)
+                newsItem = content["newsitems"][indexBase+indexInterno]
+                print("xxxxxx")
+                #print(newsItem["news_date"])
+                #newsItems = content["newsitems"]
                 news = {
-                    "description": f'Publicado em: {newsitem["news_date"]}',
-                    "url": newsitem["url"],
-                    "title": newsitem["title"],
-                }
-                newsList.append(news)
-            payload = {"content": baseContent, "username": "GamesInfo"}
-            payload["embeds"] = newsList
-        else:
-            payload = []
-            diferencial = ceil(count / 10)
-            times = 10
-            for index in range(diferencial):
-                print(f'Index {index}')
-                baseContent = f'{index+1} de {diferencial} - Existem {count} notícias sobre o app {content["appid"]} na data de {content["date"]}'
-                newsList = []
-                for newsIndex in itertools.repeat((10*index),times):
-                    newsItems = content["newsitems"]
-                    news = {
-                        "description": f'Publicado em: {newsItems[newsIndex]["news_date"]}',
-                        "url": newsItems[newsIndex]["url"],
-                        "title": newsItems[newsIndex]["title"],
+                    #"description": f'Publicado em: {newsItems[newsIndex]["news_date"]}',
+                    "description": f'Publicado em: {newsItem["news_date"]}',
+                    #"url": newsItems[newsIndex]["url"],
+                    "url": newsItem["url"],
+                    #"title": newsItems[newsIndex]["title"],
+                    "title": newsItem["title"],
                     }
-                    newsList.append(news)
-                times = count - (10 * (index + 1))
-                newsObject = {"content": baseContent, "username": "GamesInfo"}
-                newsObject["embeds"] = newsList
-                payload.append(newsObject)
+                newsList.append(news)
+            newsObject = {"content": baseContent, "username": "GamesInfo"}
+            newsObject["embeds"] = newsList
+            print(f'tamanho da lista {len(newsList)}')
+            payload.append(newsObject)
         return payload
